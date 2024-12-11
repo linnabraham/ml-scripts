f9a3ccc5c879155cc4a70a9ec03298cc0bb97f3a
linnabraham <linn.official@gmail.com>
2024-12-11 18:49:08 +0530

    Make code into an installable pip package

---
+++ README.md
@@ -0,0 +1,14 @@
+# ML Utils
+
+A package for utility functions for doing Machine Learning applications.
+
+## Install:
+```
+pip install --force-reinstall git+https://github.com/linnabraham/ml-scripts.git
+```
+
+## Usage:
+
+```
+import ml_utils
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_utils',
+    version='0.1.0',
+    author='Linn Abraham',
+    author_email='linn.official@gmail.com',
+    description='A python package for utility scripts and functions for machine learning applications',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/linnabraham/ml-scripts',  # Update with your repository URL
+    packages=find_packages(),  # Automatically finds your package
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
